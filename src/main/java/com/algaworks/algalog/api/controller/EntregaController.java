9398d9fcde14c8f38811388ad8595dd36e990c33
Netto Fernandes <you@example.com>
2021-05-27 20:01:01 -0300

    start

---
+++ src/main/java/com/algaworks/algalog/api/controller/EntregaController.java
@@ -0,0 +1,79 @@
+package com.algaworks.algalog.api.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algalog.api.assembler.EntregaAssembler;
+import com.algaworks.algalog.api.model.EntregaModel;
+import com.algaworks.algalog.api.model.input.EntregaInput;
+import com.algaworks.algalog.domain.model.Entrega;
+import com.algaworks.algalog.domain.repository.EntregaRepository;
+import com.algaworks.algalog.domain.service.FinalizacaoEntregaService;
+import com.algaworks.algalog.domain.service.SolicitacaoEntregaService;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/entregas")
+public class EntregaController {
+
+	private EntregaRepository entregaRepository;
+	private SolicitacaoEntregaService solicitacaoEntregaService;
+	private FinalizacaoEntregaService finalizacaoEntregaService;
+	private EntregaAssembler entregaAssembler;
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public EntregaModel solicitar(@Valid @RequestBody EntregaInput entregaInput) {
+		Entrega novaEntrega = entregaAssembler.toEntity(entregaInput);
+		Entrega entregaSolicitada = solicitacaoEntregaService.solicitar(novaEntrega);
+				
+		return entregaAssembler.toModel(entregaSolicitada);
+	}
+	
+	@PutMapping("/{entregaId}/finalizacao")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void finalizar(@PathVariable Long entregaId) {
+		finalizacaoEntregaService.finalizar(entregaId);
+	}
+	
+	@GetMapping
+	public List<EntregaModel> listar(){
+		return entregaAssembler.toCollectionModel(entregaRepository.findAll());
+	}
+	
+	@GetMapping("/{entregaId}")
+	public ResponseEntity<EntregaModel> buscar(@PathVariable Long entregaId){
+		return entregaRepository.findById(entregaId)
+				.map(entrega-> ResponseEntity.ok(entregaAssembler.toModel(entrega)))
+				.orElse(ResponseEntity.notFound().build());
+					
+//					EntregaModel entregaModel = new EntregaModel();
+//					entregaModel.setId(entrega.getId());
+//					entregaModel.setNomeCliente(entrega.getCliente().getNome());
+//					entregaModel.setDestinatario(new DestinatarioModel());
+//					entregaModel.getDestinatario().setNome(entrega.getDestinatario().getNome());
+//					entregaModel.getDestinatario().setLogradouro(entrega.getDestinatario().getLogradouro());
+//					entregaModel.getDestinatario().setNumero(entrega.getDestinatario().getNumero());
+//					entregaModel.getDestinatario().setComplemento(entrega.getDestinatario().getComplemento());
+//					entregaModel.getDestinatario().setBairro(entrega.getDestinatario().getBairro());
+//					entregaModel.setTaxa(entrega.getTaxa());
+//					entregaModel.setStatus(entrega.getStatus());
+//					entregaModel.setDataPedido(entrega.getDataPedido());
+//					entregaModel.setDataFinalizacao(entrega.getDataFinalizacao());
+					
+	}
+}
---
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -0,0 +1,47 @@
+package com.algaworks.algalog.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.algaworks.algalog.domain.ValidationGroups;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+@Entity
+@Table(name = "Cliente")
+public class Cliente {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nome")
+	@NotBlank
+	@Size(max = 60)
+	private String nome;
+	
+	@NotBlank
+	@Email
+	@Size(max = 255)
+	private String email;
+	
+	@Column(name = "fone")
+	@NotBlank
+	@Size(max = 20)
+	private String telefone;
+
+}
